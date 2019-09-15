b4a61f47d6557ddc747158a7834fc0c9648ae7b1
Tony Lee <tony.lee95@t-mobile.com>
2019-09-15 15:45:27 -0700

    removed unused imports

--- src/main/java/com/tmobile/cns/acms/testservice3/exceptions/handler/TestExceptionHandler.java
+++ src/main/java/com/tmobile/cns/acms/testservice3/exceptions/handler/TestExceptionHandler.java
@@ -4,14 +4,12 @@ package com.tmobile.cns.acms.testservice3.exceptions.handler;
 import com.tmobile.cns.acms.testservice3.entities.responses.BaseError;
 import com.tmobile.cns.acms.testservice3.exceptions.BadRequestException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static com.tmobile.cns.acms.testservice3.utils.helpers.buildFailResponse;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 /**
