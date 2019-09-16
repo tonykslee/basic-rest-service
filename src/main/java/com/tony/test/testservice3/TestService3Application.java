b697dcb6edea172b6d24ea7e09a6aa8abcedd39a
Tony Lee <tony.lee95@t-mobile.com>
2019-09-16 12:10:59 -0700

    renamed package and project name

---
+++ src/main/java/com/tony/test/testservice3/TestService3Application.java
@@ -0,0 +1,13 @@
+package com.tony.test.testservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestService3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestService3Application.class, args);
+    }
+
+}
